a8a7348ed315b33e03393f198b8f529ff7f14499
chenhang <1340938223@qq.com>
2023-04-21 15:42:23 +0800

    feat: 新增ciam-api入口

--- cmd/ciam-api/ciam-api.go
+++ cmd/ciam-api/ciam-api.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"github.com/chang144/ciam/internal/apiserver"
+	"github.com/chang144/golunzi/cli"
+)
 
+func main() {
+	cli.SetConfigIn("./configs/")
+	apiserver.NewApp("ciam").Run()
 }
